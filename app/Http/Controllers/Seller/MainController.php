e1bed73099f764a4345242c955109d29863dcf9a
Mohi Uddin <mohin562@gmail.com>
2021-10-13 13:35:23 +0600

    dashboard design update

--- app/Http/Controllers/Seller/MainController.php
+++ app/Http/Controllers/Seller/MainController.php
@@ -20,113 +20,111 @@ use Image;
 class MainController extends Controller
 {
 
-     public function __construct()
+    public function __construct()
     {
         $this->middleware('auth:seller');
     }
 
-       function buyerJobPost()
+    function buyerJobPost()
     {
         $jobPost = JobPost::where('status', 1)->with('Bids')->get();
         $bidderCHeck = BidderPost::where('seller_id', auth()->id())->get();
         $getGig = Gig::where('seller_id', auth()->id())
-                  ->where('status', 1)
-                  ->get();
+            ->where('status', 1)
+            ->get();
         return view('front.buyerJobPost', compact('jobPost','bidderCHeck','getGig'));
-    }   
+    }
 
     function bidRequest(Request $request)
     {
-       $this->validate($request, [
+        $this->validate($request, [
             'des' => 'required',
             'totalDay' => 'required|integer',
             'amount' => 'required|integer',
             'gig_id' => 'required',
         ]);
 
-       $bidderRequest = BidderPost::create($request->all());
+        $bidderRequest = BidderPost::create($request->all());
 
         return back()->with('status', 'Your Bid Request Successfully Posted.');
     }
 
 
-   public function sellerIndex()
-   {
+    public function sellerIndex()
+    {
+        $data = [];
+        $getUser = Seller::where('id',auth()->id())->first();
+
+        $data['sellerGig'] = Gig::where('seller_id', auth()->id())
+            ->where('status', 1)
+            ->count();
+        $data['sellerReview'] = Review::where('seller_id', auth()->id())->count();
+        $data['sellerOrder'] = Order::where('is_accept_seller',1)
+            ->where('seller_id', auth()->id())
+            ->count();
+        $data['sellerOn'] = Order::where('is_accept_seller',0)
+            ->where('seller_id', auth()->id())
+            ->count();
 
 
-    $data = [];
-    $getUser = Seller::where('id',auth()->id())->first();
+        return view('seller.dashboard', compact('data','getUser'));
+    }
+    public function sellerOrder()
+    {
+        $allorder = Order::where('seller_id', auth()->id())
+            ->where('is_accept_seller', 0)
+            ->orderBy('id', 'DESC')
+            ->get();
+        return view('seller.sellerOrder', compact('allorder'));
+    }
 
-    $data['sellerGig'] = Gig::where('seller_id', auth()->id())
-                     ->where('status', 1)
-                     ->count();
-    $data['sellerReview'] = Review::where('seller_id', auth()->id())->count();
-    $data['sellerOrder'] = Order::where('is_accept_seller',1)
-                                ->where('seller_id', auth()->id())
-                                ->count();
-    $data['sellerOn'] = Order::where('is_accept_seller',0)
-                              ->where('seller_id', auth()->id())
-                              ->count();
+    public function sellerCustomOffer()
+    {
+        $getCustomer = MessagePerson::where('seller_id', auth()->id())->get();
+        $getGig = Gig::where('seller_id', auth()->id())
+            ->get();
+        return view('seller.sellerCustomOffer', compact('getGig', 'getCustomer'));
+    }
 
-
-   	return view('seller.dashboard', compact('data','getUser'));
-   }
-	 public function sellerOrder()
-   {
-      $allorder = Order::where('seller_id', auth()->id())
-                              ->where('is_accept_seller', 0)
-                              ->orderBy('id', 'DESC')
-                              ->get();
-   	return view('seller.sellerOrder', compact('allorder'));
-   }
-
-   public function sellerCustomOffer()
-   {
-    $getCustomer = MessagePerson::where('seller_id', auth()->id())->get();
-    $getGig = Gig::where('seller_id', auth()->id())
-                  ->get();
-     return view('seller.sellerCustomOffer', compact('getGig', 'getCustomer'));
-   }
-
-   public function storeCustomOffer(Request $request)
-   {
-      $this->validate($request, [
+    public function storeCustomOffer(Request $request)
+    {
+        $this->validate($request, [
             'totalDay' => 'required|integer|',
             'amount' => 'required|integer|',
             'des' => 'required|string|'
         ]);
 
-      $customOffer = CustomOffer::create($request->all());
+        $customOffer = CustomOffer::create($request->all());
 
-       return back()->with('status', 'Your Custom Offer Are Make Successfully');
-   }
+        return back()->with('status', 'Your Custom Offer Are Make Successfully');
+    }
 
-  public function sellerReviews()
-   {
-   	return view('seller.review');
-   }
-
-
-   public function SellerPayment()
-   {
-     $paymentCustomer = PaymentSeller::where('seller_id', auth()->id())
-                                                ->orderBy('id', 'DESC')
-                                                ->get();
-      return view('seller.SellerPayment', compact('paymentCustomer'));
-   }
+    public function sellerReviews()
+    {
+        return view('seller.review');
+    }
 
 
-   public function sellerSetting()
-   {
+    public function SellerPayment()
+    {
+        $paymentCustomer = PaymentSeller::where('seller_id', auth()->id())
+            ->orderBy('id', 'DESC')
+            ->get();
+        return view('seller.SellerPayment', compact('paymentCustomer'));
+    }
 
-    $seller_id = auth()->id();
 
-    $sellerInfo = Seller::findOrfail($seller_id);
-     return view('seller.sellerSetting', compact('sellerInfo'));
-   }
-   public function sellerSettingSave(Request $request)
-   {
-      $this->validate($request, [
+    public function sellerSetting()
+    {
+
+        $seller_id = auth()->id();
+
+        $sellerInfo = Seller::findOrfail($seller_id);
+        return view('seller.sellerSetting', compact('sellerInfo'));
+    }
+    public function sellerSettingSave(Request $request)
+    {
+        $this->validate($request, [
             'name' => 'required|string|max:20',
             'email' => 'required|string|email|max:255|unique:sellers,email,'.$request->seller_id,
             'tagline' => 'required|string|max:50',
@@ -134,60 +132,60 @@ class MainController extends Controller
             'country' => 'required|string|max:20'
         ]);
 
-      $seller_id = $request->seller_id;
-      $sellerInfo = Seller::findOrfail($seller_id);
-      $userInfo = User::where('seller_id',$seller_id)->first();
+        $seller_id = $request->seller_id;
+        $sellerInfo = Seller::findOrfail($seller_id);
+        $userInfo = User::where('seller_id',$seller_id)->first();
 
-         if($request->has('profile')){
+        if($request->has('profile')){
             $image = $request->file('profile');
 
             $imageName = 'profile'.'-'.time().'.'.$image->getClientOriginalExtension();
             $post = Image::make($image)->resize(200,180)->save('storage/upload/profile/'.$imageName)->encode('jpg', 75);
             $sellerInfo->profile = $imageName;
         }else {
-              $sellerInfo->profile = $sellerInfo->profile;
-          }
+            $sellerInfo->profile = $sellerInfo->profile;
+        }
 
-      $userInfo->name = $request->name;
-      $userInfo->email = $request->email;
-
-
-
-      $sellerInfo->name = $request->name;
-      $sellerInfo->email = $request->email;
-      $sellerInfo->hour_rate = $request->hour_rate;
-      $sellerInfo->tagline = $request->tagline;
-      $sellerInfo->about = $request->about;
-      $sellerInfo->country = $request->country;
-      $sellerInfo->save();
-
-    return back();
-   }
+        $userInfo->name = $request->name;
+        $userInfo->email = $request->email;
 
 
- public function sellerSettingSavepass(Request $request)
+
+        $sellerInfo->name = $request->name;
+        $sellerInfo->email = $request->email;
+        $sellerInfo->hour_rate = $request->hour_rate;
+        $sellerInfo->tagline = $request->tagline;
+        $sellerInfo->about = $request->about;
+        $sellerInfo->country = $request->country;
+        $sellerInfo->save();
+
+        return back();
+    }
+
+
+    public function sellerSettingSavepass(Request $request)
     {
         $this->validate($request,[
             'old_password' => 'required',
             'password' => 'required|confirmed'
-          
+
         ]);
 
-     $hashPassword = Auth::user()->password;
+        $hashPassword = Auth::user()->password;
 
-       if(Hash::check($request->old_password,$hashPassword)){
-                if(!Hash::check($request->password,$hashPassword)){
-                    $user = Seller::find(Auth::id());
-                    $user->password = Hash::make($request->password);
-                    $user->save();
-                        Auth::logout();
-                        return redirect()->back()->with('status', 'Your Password has been Change');
-                }else{
-                    return redirect()->back()->with('status', 'NEw Password can not be same old password:)');
-                }
-       }else{
-           return redirect()->back()->with('status', 'You pu wrong Password try aging');
-       }
+        if(Hash::check($request->old_password,$hashPassword)){
+            if(!Hash::check($request->password,$hashPassword)){
+                $user = Seller::find(Auth::id());
+                $user->password = Hash::make($request->password);
+                $user->save();
+                Auth::logout();
+                return redirect()->back()->with('status', 'Your Password has been Change');
+            }else{
+                return redirect()->back()->with('status', 'NEw Password can not be same old password:)');
+            }
+        }else{
+            return redirect()->back()->with('status', 'You pu wrong Password try aging');
+        }
 
         return view('admin.profile');
     }
--- resources/views/seller/dashboard.blade.php
+++ resources/views/seller/dashboard.blade.php
@@ -2,133 +2,162 @@
 
 @push('css')
 
-<style>
-.alert {
-    position: relative;
-    padding: .75rem 1.25rem;
-    margin-bottom: 1rem;
-    border: 1px solid transparent;
-    border-radius: .25rem;
-}
-	.alert-primary {
-        color: #f7f7f7;
-    background-color: #006171;
-    border-color: #b8daff;
-   }
-</style>
+    <style>
+        .alert {
+            position: relative;
+            padding: .75rem 1.25rem;
+            margin-bottom: 1rem;
+            border: 1px solid transparent;
+            border-radius: .25rem;
+        }
+        .alert-primary {
+            color: #f7f7f7;
+            background-color: #006171;
+            border-color: #b8daff;
+        }
+    </style>
 @endpush
 
 @section('content')
 
-<?php
+    <?php
 
-		$gigExists = \App\Models\Gig::where('seller_id', auth()->id())->exists();
+    $gigExists = \App\Models\Gig::where('seller_id', auth()->id())->exists();
 
- ?>
+    ?>
 
-	<!-- Dashboard Content
+    <!-- Dashboard Content
 	================================================== -->
-	<div class="dashboard-content-container" data-simplebar>
-		<div class="dashboard-content-inner" >
-			@if(!$gigExists)
-			<div class="alert alert-primary" role="alert">
- 				Create your services to get work
-			</div>
-			<div class="alert alert-primary" role="alert">
- 				Go to general setting and fill personal information
-			</div>
-			@endif
+    <div class="dashboard-content-container" data-simplebar>
+        <div class="dashboard-content-inner" >
+            <!-- Dashboard Headline -->
+            <div class="dashboard-headline">
+                <h3>{{$getUser->name}}</h3>
+                <span>We are glad to see you again!</span>
 
-			<!-- Dashboard Headline -->
-			<div class="dashboard-headline">
-				<h3>{{$getUser->name}}</h3>
-				<span>We are glad to see you again!</span>
+                <!-- Breadcrumbs -->
+                <nav id="breadcrumbs" class="dark">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li>Dashboard</li>
+                    </ul>
+                </nav>
+            </div>
 
-				<!-- Breadcrumbs -->
-				<nav id="breadcrumbs" class="dark">
-					<ul>
-						<li><a href="#">Home</a></li>
-						<li>Dashboard</li>
-					</ul>
-				</nav>
-			</div>
+            <!-- Fun Facts Container -->
+            <div class="fun-facts-container">
+                <div class="fun-fact" data-fun-fact-color="#36bd78">
+                    <div class="fun-fact-text">
+                        <span>Complate Project</span>
+                        <h4>{{$data['sellerOrder']}}</h4>
+                    </div>
+                    <div class="fun-fact-icon"><i class="icon-material-outline-gavel"></i></div>
+                </div>
+                <div class="fun-fact" data-fun-fact-color="#efa80f">
+                    <div class="fun-fact-text">
+                        <span>Reviews</span>
+                        <h4>{{$data['sellerReview']}}</h4>
+                    </div>
+                    <div class="fun-fact-icon"><i class="icon-material-outline-rate-review"></i></div>
+                </div>
 
-			<!-- Fun Facts Container -->
-			<div class="fun-facts-container">
-				<div class="fun-fact" data-fun-fact-color="#36bd78">
-					<div class="fun-fact-text">
-						<span>Complate Project</span>
-						<h4>{{$data['sellerOrder']}}</h4>
-					</div>
-					<div class="fun-fact-icon"><i class="icon-material-outline-gavel"></i></div>
-				</div>
-					<div class="fun-fact" data-fun-fact-color="#36bd78">
-					<div class="fun-fact-text">
-						<span>Ongoing Project</span>
-						<h4>{{$data['sellerOn']}}</h4>
-					</div>
-					<div class="fun-fact-icon"><i class="icon-material-outline-gavel"></i></div>
-				</div>
-				<div class="fun-fact" data-fun-fact-color="#b81b7f">
-					<div class="fun-fact-text">
-						<span>Total Service</span>
-						<h4>{{$data['sellerGig']}}</h4>
-					</div>
-					<div class="fun-fact-icon"><i class="icon-material-outline-business-center"></i></div>
-				</div>
-				<div class="fun-fact" data-fun-fact-color="#efa80f">
-					<div class="fun-fact-text">
-						<span>Reviews</span>
-						<h4>{{$data['sellerReview']}}</h4>
-					</div>
-					<div class="fun-fact-icon"><i class="icon-material-outline-rate-review"></i></div>
-				</div>
+                <!-- Last one has to be hidden below 1600px, sorry :( -->
+                <div class="fun-fact" data-fun-fact-color="#2a41e6">
+                    <div class="fun-fact-text">
+                        <span>This Month Earning</span>
+                        <h4>987</h4>
+                    </div>
+                    <div class="fun-fact-icon"><i class="icon-feather-trending-up"></i></div>
+                </div>
+            </div>
+            <br>
+            <div class="fun-facts-container">
+                <div class="fun-fact" data-fun-fact-color="#36bd78">
+                    <div class="fun-fact-text">
+                        <span>Complate Project</span>
+                        <h4>{{$data['sellerOrder']}}</h4>
+                    </div>
+                    <div class="fun-fact-icon"><i class="icon-material-outline-gavel"></i></div>
+                </div>
+                <div class="fun-fact" data-fun-fact-color="#efa80f">
+                    <div class="fun-fact-text">
+                        <span>Reviews</span>
+                        <h4>{{$data['sellerReview']}}</h4>
+                    </div>
+                    <div class="fun-fact-icon"><i class="icon-material-outline-rate-review"></i></div>
+                </div>
 
-				<!-- Last one has to be hidden below 1600px, sorry :( -->
-				<div class="fun-fact" data-fun-fact-color="#2a41e6">
-					<div class="fun-fact-text">
-						<span>This Month Earning</span>
-						<h4>987</h4>
-					</div>
-					<div class="fun-fact-icon"><i class="icon-feather-trending-up"></i></div>
-				</div>
-			</div>
+                <!-- Last one has to be hidden below 1600px, sorry :( -->
+                <div class="fun-fact" data-fun-fact-color="#2a41e6">
+                    <div class="fun-fact-text">
+                        <span>This Month Earning</span>
+                        <h4>987</h4>
+                    </div>
+                    <div class="fun-fact-icon"><i class="icon-feather-trending-up"></i></div>
+                </div>
+            </div>
+            <br>
+            <div class="fun-facts-container">
+                <div class="fun-fact" data-fun-fact-color="#36bd78">
+                    <div class="fun-fact-text">
+                        <span>Complate Project</span>
+                        <h4>{{$data['sellerOrder']}}</h4>
+                    </div>
+                    <div class="fun-fact-icon"><i class="icon-material-outline-gavel"></i></div>
+                </div>
+                <div class="fun-fact" data-fun-fact-color="#efa80f">
+                    <div class="fun-fact-text">
+                        <span>Reviews</span>
+                        <h4>{{$data['sellerReview']}}</h4>
+                    </div>
+                    <div class="fun-fact-icon"><i class="icon-material-outline-rate-review"></i></div>
+                </div>
 
-			<!-- Footer -->
-			<div class="dashboard-footer-spacer"></div>
-			<div class="small-footer margin-top-15">
-				<div class="small-footer-copyrights">
-					 © 2021 <strong>Squishy Duck</strong>. All Rights Reserved.
-				</div>
-				<ul class="footer-social-links">
-					<li>
-						<a href="#" title="Facebook" data-tippy-placement="top">
-							<i class="icon-brand-facebook-f"></i>
-						</a>
-					</li>
-					<li>
-						<a href="#" title="Twitter" data-tippy-placement="top">
-							<i class="icon-brand-twitter"></i>
-						</a>
-					</li>
-					<li>
-						<a href="#" title="Google Plus" data-tippy-placement="top">
-							<i class="icon-brand-google-plus-g"></i>
-						</a>
-					</li>
-					<li>
-						<a href="#" title="LinkedIn" data-tippy-placement="top">
-							<i class="icon-brand-linkedin-in"></i>
-						</a>
-					</li>
-				</ul>
-				<div class="clearfix"></div>
-			</div>
-			<!-- Footer / End -->
+                <!-- Last one has to be hidden below 1600px, sorry :( -->
+                <div class="fun-fact" data-fun-fact-color="#2a41e6">
+                    <div class="fun-fact-text">
+                        <span>This Month Earning</span>
+                        <h4>987</h4>
+                    </div>
+                    <div class="fun-fact-icon"><i class="icon-feather-trending-up"></i></div>
+                </div>
+            </div>
 
-		</div>
-	</div>
-	<!-- Dashboard Content / End -->
+            <!-- Footer -->
+            <div class="dashboard-footer-spacer"></div>
+            <div class="small-footer margin-top-15">
+                <div class="small-footer-copyrights">
+                    © 2021 <strong>Squishy Duck</strong>. All Rights Reserved.
+                </div>
+                <ul class="footer-social-links">
+                    <li>
+                        <a href="#" title="Facebook" data-tippy-placement="top">
+                            <i class="icon-brand-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="Twitter" data-tippy-placement="top">
+                            <i class="icon-brand-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="Google Plus" data-tippy-placement="top">
+                            <i class="icon-brand-google-plus-g"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="LinkedIn" data-tippy-placement="top">
+                            <i class="icon-brand-linkedin-in"></i>
+                        </a>
+                    </li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <!-- Footer / End -->
+
+        </div>
+    </div>
+    <!-- Dashboard Content / End -->
 
 
 

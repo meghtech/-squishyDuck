fb59d2a7ad6cba5333c84493fa496f9eab5dca13
Mohi Uddin <mohin562@gmail.com>
2021-11-02 14:42:50 +0600

    implement pagination

--- resources/views/seller/market/pagination.blade.php
+++ resources/views/seller/market/pagination.blade.php
@@ -1,9 +1,11 @@
 @if($paginator->hasPages())
-    <p class="float-right">Page 1 of 10 <svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                             height="16" fill="gray" class="bi bi-caret-right-fill"
-                                             viewBox="0 0 14 14">
-            <path
-                d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+    <p class="float-right">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-caret-right-fill" viewBox="0 0 256 512">
+            <path d="M137.4 406.6l-128-127.1C3.125 272.4 0 264.2 0 255.1s3.125-16.38 9.375-22.63l128-127.1c9.156-9.156 22.91-11.9 34.88-6.943S192 115.1 192 128v255.1c0 12.94-7.781 24.62-19.75 29.58S146.5 415.8 137.4 406.6z"/>
+        </svg>
+        Page 1 of 10
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-caret-right-fill" viewBox="0 0 14 14">
+            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
         </svg>
     </p>
 @endif

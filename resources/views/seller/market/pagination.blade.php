e24e491f5808734e24ea06405cd1cf88d7a7d6bd
Mohi Uddin <mohin562@gmail.com>
2021-11-02 18:00:50 +0600

    implement pagination 3

--- resources/views/seller/market/pagination.blade.php
+++ resources/views/seller/market/pagination.blade.php
@@ -1,41 +1,33 @@
 @if($paginator->hasPages())
     <p class="float-right">
-    @if ($paginator->onFirstPage())
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-caret-right-fill" viewBox="0 0 256 512">
-                <path d="M137.4 406.6l-128-127.1C3.125 272.4 0 264.2 0 255.1s3.125-16.38 9.375-22.63l128-127.1c9.156-9.156 22.91-11.9 34.88-6.943S192 115.1 192 128v255.1c0 12.94-7.781 24.62-19.75 29.58S146.5 415.8 137.4 406.6z"/>
-            </svg>
-    @else
-        <a href="{{ $paginator->previousPageUrl() }}" rel="prev">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-caret-right-fill" viewBox="0 0 256 512">
-                <path d="M137.4 406.6l-128-127.1C3.125 272.4 0 264.2 0 255.1s3.125-16.38 9.375-22.63l128-127.1c9.156-9.156 22.91-11.9 34.88-6.943S192 115.1 192 128v255.1c0 12.94-7.781 24.62-19.75 29.58S146.5 415.8 137.4 406.6z"/>
-            </svg>
-        </a>
-    @endif
-@foreach ($elements as $element)
+        @if ($paginator->onFirstPage())
+            <i class="fa fa-caret-left" style="position: relative; top: 3px; font-size: 24px; padding-right: 5px"></i>
+        @else
+            <a class="active my-active" href="{{ $paginator->previousPageUrl() }}" rel="prev">
+                <i class="fa fa-caret-left" style="position: relative; top: 3px; font-size: 24px; padding-right: 5px"></i>
+            </a>
+        @endif
+        @foreach ($elements as $element)
 
-    @if (is_string($element))
-        <span class="disabled">{{ $element }}</span>
-    @endif
+            @if (is_string($element))
+                <span class="disabled">{{ $element }}</span>
+            @endif
 
-    @if (is_array($element))
-        @foreach ($element as $page => $url)
-            @if ($page == $paginator->currentPage())
-                <span class="active my-active">Page {{ $page }}</span></li>
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <span >Page {{ $page }}</span></li>
+                    @endif
+                @endforeach
+                of {{ $paginator->lastPage() }}
             @endif
         @endforeach
-            of {{ $paginator->lastPage() }}
-    @endif
-@endforeach
-    @if ($paginator->hasMorePages())
-        <a href="{{ $paginator->nextPageUrl() }}" rel="next">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-caret-right-fill" viewBox="0 0 14 14">
-                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
-            </svg>
-        </a>
-    @else
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-caret-right-fill" viewBox="0 0 14 14">
-                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
-            </svg>
-    @endif
+        @if ($paginator->hasMorePages())
+            <a class="active my-active" href="{{ $paginator->nextPageUrl() }}" rel="next">
+                <i class="fa fa-caret-right" style="position: relative; top: 3px; font-size: 24px; padding-left: 5px"></i>
+            </a>
+        @else
+            <i  class="fa fa-caret-right" style="position: relative; top: 3px; font-size: 24px; padding-left: 5px"></i>
+        @endif
     </p>
 @endif
